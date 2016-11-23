1decbd11c067f2b761f0269d34483eb311df49e6
yiming <153394963@qq.com>
2016-11-23 16:02:19 +0800

    111

--- spring-cloud-demo-parent/spring-cloud-server-eureka/src/main/java/me/cloud/DiscoveryApplication.java
+++ spring-cloud-demo-parent/spring-cloud-server-eureka/src/main/java/me/cloud/DiscoveryApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryApplication {
+//    111
 	public static void main(String[] args) {
         SpringApplication.run(DiscoveryApplication.class,args);
     }
